275bd35e972eed48cee7e2db46a17261ea79d8fd
Guillaume Charpentier <guillaume.prodancer@gmail.com>
2024-10-07 14:19:47 +0200

    Add Backend files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
+import SignIn from "./pages/Signin";
 import UserAccounts from "./pages/UserAccounts";
 
 function App() {
